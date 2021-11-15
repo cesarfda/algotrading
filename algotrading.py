76f38276b782b03901bd28159353749c225974db
Cesar Araujo <56365373+cesarfda@users.noreply.github.com>
2021-11-15 09:21:48 -0700

    Update algotrading.py
    added comments

--- algotrading.py
+++ algotrading.py
@@ -3,14 +3,19 @@ import numpy as np
 import time
 import os
 
-PUB_KEY = os.environ['PUB_KEY']
+'''
+Creating connectiong with Alpaca API to retrieve market data and place orders
+'''
+KEY_ID = os.environ['PUB_KEY']
 SEC_KEY = os.environ['SEC_KEY']
 BASE_URL = 'https://paper-api.alpaca.markets'
-api = tradeapi.REST(key_id= PUB_KEY, secret_key=SEC_KEY, base_url=BASE_URL)
+api = tradeapi.REST(key_id= KEY_ID, secret_key=SEC_KEY, base_url=BASE_URL)
 
+'''
+Retrieve market data in the format of a numpy array of the last 15 minutes of price activity for a given ticker
+'''
 def get_data():
-    # Returns a an numpy array of the closing prices of the past 5 minutes
-    market_data = api.get_barset(symb, 'minute', limit=5)
+    market_data = api.get_barset(symb, 'minute', limit=15)
     
     close_list = []
     for bar in market_data[symb]:
@@ -20,6 +25,9 @@ def get_data():
 
     return close_list
 
+'''
+Place a buy order on alpaca for a given symbol and quantity
+'''
 def buy(q, s): # Returns nothing, makes call to buy stock
     api.submit_order(
         symbol=s,
@@ -28,6 +36,10 @@ def buy(q, s): # Returns nothing, makes call to buy stock
         type='market',
         time_in_force='gtc'
     )
+
+'''
+Place a sell order on alpaca for a given symbol and quantity
+'''
 def sell(q, s): # Returns nothing, makes call to sell stock
     api.submit_order(
         symbol=s,
@@ -37,9 +49,17 @@ def sell(q, s): # Returns nothing, makes call to sell stock
         time_in_force='gtc'
     )
 
+'''
+Determine what ticker will be used to trade
+'''
+
 symb = "SPY" # Ticker of stock you want to trade
 pos_held = False
 
+'''
+Loop to execute the algortighm. The algorithm only executes if the market is open, otherwise it sleep for 30 minutes and checks
+if the market is open again before trying to check prices or place orders
+'''
 while True:
     
     clock = api.get_clock()
@@ -51,20 +71,17 @@ while True:
       close_list = get_data()
 
       ma = np.mean(close_list)
-      last_price = close_list[4]
+      last_price = close_list[-1]
 
       print("Moving Average: " + str(ma))
       print("Last Price: " + str(last_price))
 
-      # Make buy/sell decision
-      # This algorithm buys or sells when the moving average crosses the most recent closing price 
-
-      if ma + 0.1 < last_price and not pos_held: # Buy when moving average is ten cents below the last price
+      if ma + 0.1 < last_price and not pos_held:
           print("Buy")
           buy(25, symb)
           pos_held = True
       
-      elif ma - 0.1 > last_price and pos_held: # Sell when moving average is ten cents above the last price
+      elif ma - 0.1 > last_price and pos_held:
           print("Sell")
           sell(25, symb)
           pos_held = False
